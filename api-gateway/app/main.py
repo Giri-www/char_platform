91d41d48fa3e90b0f0ea5dfda140cb29da2c6556
giri-www <girirahul944@gmail.com>
2026-04-03 12:27:05 +0530

    FEAT(api_gateway) : main.py code added

---
+++ api-gateway/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from routers import auth_routers, user_routers, chat_routers
+
+app = FastAPI()
+
+app.include_router(auth_routers.router)
+app.include_router(user_routers.router)
+app.include_router(chat_routers.router)
+
+
+@app.get("/")
+def home():
+    return {"message": "API Gateway Running"}
